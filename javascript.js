dd6d3437c02be36d0969d3b32da0981bf412c340
G-Alessandro <galloalessandro77@gmail.com>
2023-04-03 12:38:15 +0200

    Add basic html and css

---
+++ javascript.js
@@ -0,0 +1,21 @@
+const bookContainer = document.getElementByClassName('bookContainer');
+const subBtn = document.getElementsByClassName('subBtn');
+
+// let myLibrary = [a,b,c,d];
+
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+// function addBookToLibrary() {
+
+// }
+
+// bookContainer.appendChild(myLibrary)
+
+// bookContainer.setAttribute('style', 'background: blue;');
+
+bookContainer.textContent = 'hello world';
