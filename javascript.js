7e1d18ecd78cdda8dd00932659cd287f0227c962
G-Alessandro <galloalessandro77@gmail.com>
2023-04-03 21:18:33 +0200

    Add function addBookToLibrary()

--- javascript.js
+++ javascript.js
@@ -1,21 +1,28 @@
-const bookContainer = document.getElementByClassName('bookContainer');
-const subBtn = document.getElementsByClassName('subBtn');
+const bookContainer = document.getElementById('bookContainer');
+const subBtn = document.getElementById('subBtn');
 
-// let myLibrary = [a,b,c,d];
+const myLibrary = [];
 
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-// function addBookToLibrary() {
+function addBookToLibrary() {
+  const t = document.getElementById('title').value;
+  const a = document.getElementById('author').value;
+  const p = document.getElementById('pages').value;
+  const r = document.getElementById('read').value;
+  const newBook = new Book(t, a, p, r);
+  myLibrary.push(newBook);
+  // document.getElementById('bookContainer').innerText = x;
+  console.log(myLibrary);
+}
 
-// }
-
-// bookContainer.appendChild(myLibrary)
-
-// bookContainer.setAttribute('style', 'background: blue;');
-
-bookContainer.textContent = 'hello world';
+subBtn.addEventListener('click', () => {
+  addBookToLibrary();
+  console.log('1', myLibrary[0]);
+  console.log('2', myLibrary[0].title);
+});
