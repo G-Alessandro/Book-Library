3b79bc589a6c8c7b895dbfb0e901c2e1308e3e9b
G-Alessandro <galloalessandro77@gmail.com>
2023-04-04 18:21:41 +0200

    Add function addBookDiv

--- javascript.js
+++ javascript.js
@@ -1,24 +1,27 @@
 const bookContainer = document.getElementById('bookContainer');
-const bookAdd = document.createElement('div');
 const subBtn = document.getElementById('subBtn');
 
 let myLibrary = [{title: 'The Lord of The Rings', author: 'John R.R. Tolkien', pages: '1178', read: 'No' },
-                  {title: 'The Physics of Star Trek', author: 'Lawrence M. Krauss, Stephen Hawking (Foreword)', pages: '208', read: 'No' },
+{title: 'The Physics of Star Trek', author: 'Lawrence M. Krauss, Stephen Hawking (Foreword)', pages: '208', read: 'No' },
                   {title: 'La scienza delle verdure', author: 'Dario Bressanini', pages: '277', read: 'No'},
                   {title: 'La scienza della carne', author: 'Dario Bressanini', pages: '239', read: 'No'},
                   {title: 'A piedi nudi su Marte', author: 'Adrian Fartade', pages: '285', read: 'No'}
-];
-
-function Book(title, author, pages, read) {
-  this.title = title;
+                ];
+                
+                function Book(title, author, pages, read) {
+                  this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-// function addBook {
-//   for ()
-// }
+function addBookDiv() {
+  for (book in myLibrary) {
+    const bookAdd = document.createElement('div');
+    bookAdd.classList.add('bookInLibrary');
+    bookContainer.appendChild(bookAdd);
+  }
+}
 
 function addBookToLibrary() {
   let t = document.getElementById('title').value;
@@ -27,7 +30,7 @@ function addBookToLibrary() {
   let r = document.getElementById('read').value;
   let x = new Book(t, a, p, r);
   myLibrary.push(x);
-
+  addBookDiv()
   console.log(myLibrary);
 }
 
@@ -35,7 +38,6 @@ function addBookToLibrary() {
 subBtn.addEventListener('click', () => {
   addBookToLibrary();
 
-  bookContainer.appendChild(bookAdd);
   console.log('111', myLibrary[0]);
   console.log('222', myLibrary.length);
 });
